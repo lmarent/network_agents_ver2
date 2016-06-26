4fcfa8654706fe4569cf0691d6aca0f3f9140671
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2016-06-26 08:07:42 -0500

    cost function implementation

---
+++ CostFunctions/include/MiscCostModule.h
@@ -0,0 +1,38 @@
+/*! \file  MiscCostModule.h
+
+    Copyright 2014-2016 Universidad de los Andes, Bogota, Colombia
+                        University of Massachusetts, Amherst  			
+
+    This file is part of the ChoiceNet Network Simulation System.
+
+    NETQos is free software; you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published by 
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    NETQoS is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this software; if not, write to the Free Software 
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    $Id: MiscCostModule.h 748 2016-06-25 8:35:00 amarentes $
+
+*/
+
+
+#ifndef _MISC_COST_MODULE_H_
+#define _MISC_COST_MODULE_H_
+
+
+#include "CostModuleInterface.h"
+
+
+/*! \short   declaration of struct containing all function pointers of a module */
+extern ChoiceNet::Eco::CostModuleInterface_t func;
+
+
+#endif /* _MISC_COST_MODULE_H_ */
